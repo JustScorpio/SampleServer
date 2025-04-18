e8ec7232b2714f868d38310b7c425d9a709aa759
Kosarev <skorpiello@gmail.com>
2025-04-18 23:38:17 +0300

    task 4/8

--- main.go
+++ main.go
@@ -11,8 +11,13 @@ func mainPage(res http.ResponseWriter, req *http.Request) {
 	for k, v := range req.Header {
 		body += fmt.Sprintf("%s: %v\r\n", k, v)
 	}
+
+	err := req.ParseForm()
+	if err != nil {
+		panic(err)
+	}
 	body += "Query parameters ===============\r\n"
-	for k, v := range req.URL.Query() {
+	for k, v := range req.Form {
 		body += fmt.Sprintf("%s: %v\r\n", k, v)
 	}
 	res.Write([]byte(body))
